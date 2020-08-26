37811981d06ef30e6dd311aa131c2cc896ffef32
kev127 <kelvinkeya125@gmail.com>
2020-08-26 20:18:08 +0300

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
