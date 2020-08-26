7d8299e48581d3f6814a5bc3ce86a1f27d22f370
kev127 <kelvinkeya125@gmail.com>
2020-08-26 21:48:53 +0300

    modified constructor in quote-details and quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,10 @@
 export class Quote {
-    constructor(public author:string, public quote:string,public  date:Date){
-      
+    public showDescription:boolean;
+    public upvotes:number;
+    public downvotes:number;
+    constructor(public text: string,public author: string,public completeDate: Date ,public poster:string){
+        this.showDescription=false;
+        this.upvotes=0;
+        this.downvotes=0;
     }
-  }
+}
